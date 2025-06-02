2815db0c92abaeb1a146f268115df40be47ce3c6
ghostkillaxreal <petrperviyleon@mail.ru>
2025-06-02 21:09:17 +0500

    Добавлена проверка сложности пароля

--- password_generator.py
+++ password_generator.py
@@ -2,17 +2,21 @@ import random
 import string
 import argparse
 
-def generate_password(length=12, no_special=False, exclude_ambiguous=False):
-    # ... (предыдущая реализация функции)
+# ... (предыдущие функции)
+
+def check_strength(password):
+    """Проверяет сложность пароля"""
+    strength = 0
+    if any(c.islower() for c in password): strength += 1
+    if any(c.isupper() for c in password): strength += 1
+    if any(c.isdigit() for c in password): strength += 1
+    if any(c in "!@#$%^&*()_+-=" for c in password): strength += 1
+    return strength
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-l", "--length", type=int, default=12, help="Длина пароля")
-    parser.add_argument("-n", "--no-special", action="store_true", help="Без спецсимволов")
-    parser.add_argument("-a", "--exclude-ambiguous", action="store_true", help="Исключить неоднозначные символы")
-    parser.add_argument("-c", "--count", type=int, default=1, help="Количество паролей")
-    args = parser.parse_args()
+    # ... (предыдущий код аргументов)
     
     for i in range(args.count):
         password = generate_password(args.length, args.no_special, args.exclude_ambiguous)
-        print(f"Пароль {i+1}: {password}")
+        strength = check_strength(password)
+        print(f"Пароль {i+1} ({strength}/4): {password}")
